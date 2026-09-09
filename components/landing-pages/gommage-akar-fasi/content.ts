fa9115f31d55830f55847449ef885e38c022b519
mohamednafii <mohamed.nafii160@gmail.com>
2026-09-09 17:11:11 +0100

    changing price

--- components/landing-pages/gommage-akar-fasi/content.ts
+++ components/landing-pages/gommage-akar-fasi/content.ts
@@ -99,18 +99,18 @@ export const gommageAkarFasi: LandingContent = {
   ],
 
   packs: [
-    { id: "single", name: "علبة واحدة", price: 150, image: { src: `${A}/product/pack1.webp`, width: 137, height: 170 } },
+    { id: "single", name: "علبة واحدة", price: 176, image: { src: `${A}/product/pack1.webp`, width: 137, height: 170 } },
     {
       id: "double",
       name: "علبتان",
-      price: 275,
+      price: 352,
       badge: "الأكثر طلباً",
       image: { src: `${A}/product/pack2.webp`, width: 170, height: 110 },
     },
     {
       id: "triple",
       name: "3 علب",
-      price: 400,
+      price: 528,
       badge: "الأفضل قيمة",
       image: { src: `${A}/product/pack3.webp`, width: 170, height: 118 },
     },
